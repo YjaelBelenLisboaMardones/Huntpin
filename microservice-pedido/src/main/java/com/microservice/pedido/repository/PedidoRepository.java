8f2e186c9d6376e6615e7ecd56fa3d0978ba2e66
yja <yj.lisboa@duocuc.cl>
2025-06-27 12:01:01 -0400

    msvc-producto ok

--- microservice-pedido/src/main/java/com/microservice/pedido/repository/PedidoRepository.java
+++ microservice-pedido/src/main/java/com/microservice/pedido/repository/PedidoRepository.java
@@ -5,11 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido,Integer>{
     
-    // Buscar todos los pedidos de un cliente específico
-    List<Pedido> findByIdCliente(int idCliente);
 }
