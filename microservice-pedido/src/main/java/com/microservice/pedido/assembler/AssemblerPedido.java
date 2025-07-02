23dd72191c48ad9addd459b2f2770a2eb7daaa1c
yja <yj.lisboa@duocuc.cl>
2025-07-02 10:52:48 -0400

    assemblers ok

--- microservice-pedido/src/main/java/com/microservice/pedido/assembler/AssemblerPedido.java
+++ microservice-pedido/src/main/java/com/microservice/pedido/assembler/AssemblerPedido.java
@@ -6,13 +6,15 @@ import com.microservice.pedido.controller.PedidoController;
 import com.microservice.pedido.model.Pedido;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AssemblerPedido implements RepresentationModelAssembler<Pedido, EntityModel<Pedido>> {
 
     @Override
-    public EntityModel<Pedido> toModel(Pedido pedido) {
+    @NonNull
+    public EntityModel<Pedido> toModel(@NonNull Pedido pedido) {
         return EntityModel.of(pedido,
                 linkTo(methodOn(PedidoController.class).getProductoById(pedido.getIdPedido())).withSelfRel(),
                 linkTo(methodOn(PedidoController.class).getAllPedidos()).withRel("pedidos"));
--- microservice-producto/src/main/java/com/microservice/producto/assembler/AssemblerProducto.java
+++ microservice-producto/src/main/java/com/microservice/producto/assembler/AssemblerProducto.java
@@ -6,13 +6,15 @@ import com.microservice.producto.controller.ProductoController;
 import com.microservice.producto.model.Producto;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AssemblerProducto implements RepresentationModelAssembler<Producto, EntityModel<Producto>> {
 
     @Override
-    public EntityModel<Producto> toModel(Producto producto) {
+    @NonNull
+    public EntityModel<Producto> toModel(@NonNull Producto producto) {
         return EntityModel.of(producto,
                 linkTo(methodOn(ProductoController.class).getProductoById(producto.getId_producto())).withSelfRel(),
                 linkTo(methodOn(ProductoController.class).getAllProducts()).withRel("productos"));
