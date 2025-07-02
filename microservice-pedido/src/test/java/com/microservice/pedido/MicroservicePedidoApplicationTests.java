f78eb0f716bc5724dfd1fd2fa5161de95ba7f7dd
yja <yj.lisboa@duocuc.cl>
2025-07-02 06:15:12 -0400

    change testmain pedido's namw

--- microservice-pedido/src/test/java/com/microservice/pedido/MicroservicePedidoApplicationTests.java
+++ microservice-pedido/src/test/java/com/microservice/pedido/MicroservicePedidoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroservicPedidoApplicationTests {
+class MicroservicePedidoApplicationTests {
 
 	@Test
 	void contextLoads() {
